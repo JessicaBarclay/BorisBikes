0c73d6a98f4ccd0ae88003f30773ac52393749a9
david gray <davidseangray@gmail.com>
2017-04-11 18:02:48 +0100

    failure testing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,13 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
+
   end
 
   it 'responds to release_bike' do
